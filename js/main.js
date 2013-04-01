4bdb579b2d5268f036ca648832cbcf63cc4363d6
Lauren Middleton <git@laurenmiddleton.com>
2013-03-31 20:33:31 -0400

    added basic map, styles, filter outlines

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 /*Project 2 functions*/
 
 window.onload = function() {
+	//init map 1
+	$('#map1').datamap({
+		scope: 'usa',
+		fills: {
+			defaultFill: 'EFEFEF'
+		}
+	})
+
+	//for testing
 	alert("javascript is working.");
 }
