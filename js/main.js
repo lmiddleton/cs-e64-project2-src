b745a9c4ca31d184d66d9741926ecf7da457ae0b
Lauren Middleton <git@laurenmiddleton.com>
2013-04-04 21:23:58 -0400

    modified drawMap function to avoid duplicates

--- js/main.js
+++ js/main.js
@@ -94,15 +94,36 @@ function parseCsv(filepath) {
 */
 
 /*re-renders the map for the selected year*/
-function drawMapForYear(map, year, dataObject) {
+function drawMap(year, dataObject) {
+	//clear map div so map is not duplicated
+	$("#map1").empty();
+
 	console.log(year);
+	var newTemplate = '<div class="hoverinfo"><strong><%= geography.properties.name %></strong> <% if (data["2006"]["Handguns"]) { %><hr/>  Total Handguns: <%= data["2006"]["Handguns"] %> <% } %></div>';
+
+	var map = new Map({
+		scope: 'usa',
+		el: $('#map1'),
+      	geography_config: { 
+        	highlightBorderColor: '#FFFF00',
+        	highlightOnHover: true,
+        	popupTemplate: _.template(newTemplate)
+      },
+		fills: {
+			'TEST1': '#000000',
+			'TEST2': '#FFFFFF',
+			defaultFill: '#EFEFEF'
+		},
+		data: JSON_data
+	});
+
 	for(state in dataObject) {
 		if (!dataObject.hasOwnProperty(state)){
 			continue;
 		}
 		var totalMurders = dataObject[state][year]["Total murders1"];
 		//console.log(totalMurders);
-		if(totalMurders >= 50){
+		if(totalMurders >= 40){
 			//one color
 			dataObject[state]["fillKey"] = "TEST1";
 		}
@@ -117,12 +138,12 @@ function drawMapForYear(map, year, dataObject) {
 }
 
 /*year dropdown handler*/
-function initYearSelect(map) {
+function initYearSelect() {
 	$("#year").change(function() {
 		//find which year was selected
 		var year = this.value;
 		//redraw the map based on that year
-		drawMapForYear(map, year, JSON_data);
+		drawMap(year, JSON_data);
 
 		alert(year);
 	});
@@ -132,36 +153,30 @@ function initYearSelect(map) {
 
 window.onload = function() {
 	//init map 1
-	var map = new Map({
-		scope: 'usa',
-		el: $('#map1'),
-      	geography_config: { 
-        	highlightBorderColor: '#FFFF00',
-        	highlightOnHover: true,  // For reason I don't understand you don't reference by state //It's just a temp hack, I couldn't get it to work w/o putting in something there, not sure why it's working in yours, maybe b/c you copied the JSON object right into data and I'm referencing a variable?
-        	popupTemplate: _.template('<div class="hoverinfo"><strong><%= geography.properties.name %></strong> <% if (JSON_data["AK"]["2006"]["Total murders1"]) { %><hr/>  Total Murders: <%= JSON_data["AK"]["2006"]["Total murders1"] %> <% } %></div>')
-      },
-		fills: {
-			'TEST1': '#000000',
-			'TEST2': '#FFFFFF',
-			defaultFill: '#EFEFEF'
-		},
-		data: JSON_data
-	});
+	
 
 	//how to access data in JSON object
 	var test = JSON_data["AK"]["2006"]["Handguns"];
 	console.log(test);
 
-	drawMapForYear(map, "2006", JSON_data);
+	drawMap("2006", JSON_data);
 
-	initYearSelect(map);
+	newTemplate = '<div class="hoverinfo"><strong><%= geography.properties.name %></strong> <% if (data["2007"]["Handguns"]) { %><hr/>  Total Handguns: <%= data["2007"]["Handguns"] %> <% } %></div>';
+
+	initYearSelect();
 
 	//to then modify the object (aka change the fill)
 	//JSON_data["AK"]["fillKey"] = "TEST1";
 	//console.log(JSON_data);
 
 	//to change the popupTemplate (ex. for a different year)
-	//var template = map.geography_config;
+	
+	//map.render;
+
+
+	//map.options.geography_config.popupTemplate.template = newTemplate;
+	//console.log(map.options.geography_config.popupTemplate.template);
+	//map.render();
 	//console.log(template);
 
 	
